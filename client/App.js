1d9f8a691e2b48ed24c6312acc1f998f0c35cf83
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-08-12 23:45:07 +0200

    Add App renderLayout method

--- client/App.js
+++ client/App.js
@@ -5,9 +5,14 @@ import io from 'socket.io-client';
 const socket = io('/');
 
 class App extends Component {
+    
     constructor(props) {
         super(props);
         this.state = { users: [], messages: [], text: '', name: '' };
+    }
+
+    render() {
+        return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
     }
 };
 export default hot(module)(App);
