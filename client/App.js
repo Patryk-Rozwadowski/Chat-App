52f57218750b80138d12c66233292c18d37bd3ae
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-08-12 23:29:49 +0200

    Add index.js App.js, App class

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import io from 'socket.io-client';
+
+const socket = io('/');
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { users: [], messages: [], text: '', name: '' };
+    }
+};
+export default hot(module)(App);
