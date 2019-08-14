29f7f3bad56df7531173b15f33636999a10c2e37
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-08-15 01:11:12 +0200

    Fixed webpack cfg

--- webpack.config.js
+++ webpack.config.js
@@ -28,40 +28,41 @@ module.exports = (env) => {
             path: path.resolve(__dirname, 'public'),
             filename: 'app.bundle.js'
         },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    options: {
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                    }
+                },
+
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        optimization: {
+            minimizer: [new UglifyJsPlugin()],
+        },
+        plugins: plugins,
         devServer: {
             proxy: {
                 '/socket.io': {
                     target: 'http://localhost:3000',
                     ws: true
                 }
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.js$/,
-                        loader: "babel-loader",
-                        options: {
-                            plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
-                        }
-                    },
-                    {
-                        test: /\.css$/,
-                        use: [
-                            { loader: 'style-loader' },
-                            {
-                                loader: 'css-loader',
-                                options: {
-                                    modules: true
-                                }
-                            }
-                        ]
-                    }
-                ]
-            },
-            optimization: {
-                minimizer: [new UglifyJsPlugin()],
-            },
-            plugins: plugins
+            }
         }
-    };
-}
+    }
+};
