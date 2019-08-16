f00dc05fa41dc733834369878d564c5049d7d471
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-08-17 01:32:45 +0200

    Fixed styles for messages

--- client/MessageList.js
+++ client/MessageList.js
@@ -3,9 +3,9 @@ import React from 'react';
 import styles from './MessageList.css';
 
 const Message = props => (
-    <div className={styles.Message}>
-        <strong>{props.from} :</strong>
-        <span>{props.text}</span>
+    <div className={styles.MessageContainer}>
+        <span className={styles.From}>{props.from} :</span>
+        <span className={styles.Message}>{props.text}</span>
     </div>
 );
 
